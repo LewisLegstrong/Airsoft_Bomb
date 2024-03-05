820d133295b30a4bde812367edac23afabee5f16
LewisLegstrong <luis.duarte@outlook.pt>
2024-03-05 18:57:14 +0000

    First commit - timer, button and task creation

---
+++ README.md
@@ -0,0 +1,43 @@
+| Microcontroller | ESP-Wroom-32 |
+| --------------- | ------------ |
+
+# Airsoft Bomb
+
+## Index
+
+1. [Inspiration](#inspiration)
+2. [Supported Targets](#supported-targets)
+3. [Airsoft Bomb](#airsoft-bomb)
+
+---
+
+## Inspiration
+
+Watching and knowing a mate of mine was playing Airsoft as a past time activity, and having participated a few times myself, being a CS GO and CS 2 player, I thought of a cool project to create a bomb (Airsoft) and have it as the center of Airsoft events.
+
+I will be capable of joining my CAD and 3D printing experience to programming and build a product from the ground up.
+
+This also offers the opportunity to tackle a project with a certain level of complexity for educational purposes, allowing me to extend my knowledge with embedded systems, multithreading and accurate use of limited resources when coding.
+
+---
+
+## Defining goals and objectives
+
+First things first: this project will use timers, buzzers, buttons and LEDs as the basic to operate, so getting some templates for those to work is mandatory. While doing so, getting tasks to run is also a good point.
+
+---
+
+## Getting Familiar again
+
+From a past experience using the ESP32 as a microcontroller, and creating code for it with ESP-IDF, I know I
+
+![NodeMCU ESP32 pinout](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instructables.com%2FESP32-Internal-Details-and-Pinout%2F&psig=AOvVaw1wFCY6qIgE8HKRVq_NJquY&ust=1709749355577000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDsjOre3YQDFQAAAAAdAAAAABAX)
+
+---
+
+
+---
+
+
+
+---
---
+++ main/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_timer.h"
+#include "driver/gpio.h"
+
+typedef struct  round_timer_t{
+	uint32_t round_time_s;
+} round_timer_t;
+
+void task_user_input() {
+	gpio_config_t io_conf = {};
+		io_conf.intr_type = GPIO_INTR_DISABLE;// disable interrupt
+		io_conf.mode = GPIO_MODE_INPUT;// set as output mode
+		io_conf.pin_bit_mask = (1ULL<<GPIO_NUM_39);// bit mask of the pins that you want to set
+		io_conf.pull_down_en = 0;// disable pull-down mode
+		io_conf.pull_up_en = 0;// disable pull-up mode
+	gpio_config(&io_conf);	// configure GPIO with the given settings
+	while(1){
+		if (!gpio_get_level(GPIO_NUM_39)){
+			printf("Button pressed\n");
+		}
+		vTaskDelay(10 / portTICK_PERIOD_MS);
+	}
+}
+
+void task_user_time(void *roundTime) {
+	round_timer_t* params = (round_timer_t *)roundTime;
+	uint32_t round_time_s = params->round_time_s;
+	uint32_t start_time = esp_timer_get_time();
+	uint32_t end_time = start_time + round_time_s * 1000000;
+
+	while(1){
+		printf("Round started \n");
+		while(esp_timer_get_time() < end_time){
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+		}
+		printf("Round ended \n");
+		start_time = esp_timer_get_time();
+		end_time = start_time + round_time_s * 1000000;
+	}
+}
+
+void app_main(void) {
+	round_timer_t roundTime;
+	roundTime.round_time_s = 5;
+	xTaskCreate(task_user_time, "task_user_time", 2048, &roundTime, 5, NULL);
+	xTaskCreate(task_user_input, "task_user_input", 2048, NULL, 5, NULL);
+}
