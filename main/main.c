355ceade37f85018f52c50651743fd0ffb29226a
LewisLegstrong <luis.duarte@outlook.pt>
2024-05-27 10:40:17 +0100

    -Change driver used for PWM changed to LEDC

--- main/main.c
+++ main/main.c
@@ -3,12 +3,18 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_timer.h"
-#include "driver/gpio.h"
 
-typedef struct  round_timer_t{
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#define PWM_FREQUENCY 2000 // PWM signal frequency in Hz
+#define PWM_DUTY 50		   // PWM duty cycle in percentage (0-100)
+
+typedef struct  time_param{
 	uint32_t round_time_s;
 } round_timer_t;
 
@@ -28,15 +34,18 @@ void task_user_input() {
 	}
 }
 
-void task_user_time(void *roundTime) {
-	round_timer_t* params = (round_timer_t *)roundTime;
+void task_user_time(void *roundTime)
+{
+	round_timer_t *params = (round_timer_t *)roundTime;
 	uint32_t round_time_s = params->round_time_s;
 	uint32_t start_time = esp_timer_get_time();
 	uint32_t end_time = start_time + round_time_s * 1000000;
 
-	while(1){
+	while (1)
+	{
 		printf("Round started \n");
-		while(esp_timer_get_time() < end_time){
+		while (esp_timer_get_time() < end_time)
+		{
 			vTaskDelay(1000 / portTICK_PERIOD_MS);
 		}
 		printf("Round ended \n");
@@ -45,9 +54,42 @@ void task_user_time(void *roundTime) {
 	}
 }
 
-void app_main(void) {
+void pwm_task(void *arg){
+	ledc_timer_config_t ledc_timer = {
+		.speed_mode = LEDC_HIGH_SPEED_MODE,
+		.duty_resolution = LEDC_TIMER_10_BIT,
+		.timer_num = LEDC_TIMER_0,
+		.freq_hz = PWM_FREQUENCY
+	};
+	ledc_timer_config(&ledc_timer);
+
+	ledc_channel_config_t ledc_channel = {
+		.channel = LEDC_CHANNEL_0,
+		.duty = 0,
+		.gpio_num = GPIO_NUM_27,
+		.speed_mode = LEDC_HIGH_SPEED_MODE,
+		.timer_sel = LEDC_TIMER_0
+	};
+	ledc_channel_config(&ledc_channel);
+
+	while(1){
+		//make a sound for 500ms
+		ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0);
+		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+
+		ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, PWM_DUTY);
+		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+		vTaskDelay(200 / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main(void)
+{
 	round_timer_t roundTime;
 	roundTime.round_time_s = 5;
 	xTaskCreate(task_user_time, "task_user_time", 2048, &roundTime, 5, NULL);
 	xTaskCreate(task_user_input, "task_user_input", 2048, NULL, 5, NULL);
+	xTaskCreate(pwm_task, "pwm_task", 2048, NULL, 5, NULL);
 }
+
